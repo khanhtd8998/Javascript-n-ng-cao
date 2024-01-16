35798fc7de191831e07fcdadb382a30915d03261
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-01-16 10:55:43 +0700

    coding lab 2.3 + 2.4

--- Data Structure and Modern Operator/Lab2/main.js
+++ Data Structure and Modern Operator/Lab2/main.js
@@ -29,31 +29,46 @@ for (const [key, value] of game) {
 }
 
 //#Coding Chellenge #4
-// const arr = 'khanh_tran_duy'
-// const arr = str.split('_');
-// const arr2 = [];
-// arr2.push(arr[0]);
-// for(let i = 1; i < arr.length; i++) {
-//     arr2.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1));
-// }
-// console.log(arr2);
-// console.log(arr2.join(""));
-
-
 const form = document.querySelector('form');
 const string = document.querySelector('#stringValue');
 const showString = document.querySelector('h2');
-form.addEventListener('submit',function (event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     changeStr();
 });
 function changeStr() {
     const stringValue = string.value;
     const arr = stringValue.split('\n');
-    for(const i of arr) {
-        const [a,b] = i.toLowerCase().trim().split('_');
-        const output = `${a}${b.replace(b[0],b[0].toUpperCase())}`;
+    for (const i of arr) {
+        const [a, b] = i.toLowerCase().trim().split('_');
+        const output = `${a}${b.replace(b[0], b[0].toUpperCase())}`;
         console.log(output);
     }
-    
+
 }
+
+//Lab2.3
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+console.log(getCode);
+for (const flight of flights.split('+')) {
+    console.log(flight);
+    const [type, from, to, time] = flight.split(';');
+    console.log(type, from, to, time);
+    const output = `${type.startsWith('_Delayed') ? '�' : ''}${type.replaceAll('_',' ')} 
+    ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+    console.log(output);
+
+}
+
+//Lab2.4
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('body').addEventListener
+        ('click', function () {
+            setTimeout(() => header.style.color = 'blue', 3000);
+        });
+})();
